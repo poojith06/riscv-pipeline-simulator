abdb15c2a094b6e87f14a738b3f8641161c68df3
poojith06 <saipoojith2006@gmail.com>
2026-03-09 16:50:34 +0530

    Minor changes

--- EX_MEM.v
+++ EX_MEM.v
@@ -1,50 +1,50 @@
 module EX_MEM(
-    input clk,reset,mem_to_reg,reg_write_en,mem_read,mem_write,
-    input[63:0] alu_out,data,
-    input[4:0] rs2_ID_EX,rd,
-    output mem_to_reg_out,reg_write_en_out,mem_read_out,mem_write_out,
-    output[63:0] alu_out_out,data_out,
-    output[4:0] rs2_ID_EX_out,rd_out
+    input clk,reset,EX_MEM_mem_to_reg,EX_MEM_reg_write_en,EX_MEM_mem_read,EX_MEM_mem_write,
+    input[63:0] EX_MEM_alu_out,EX_MEM_data,
+    input[4:0] EX_MEM_rs2_ID_EX,EX_MEM_rd,
+    output EX_MEM_mem_to_reg_out,EX_MEM_reg_write_en_out,EX_MEM_mem_read_out,EX_MEM_mem_write_out,
+    output[63:0] EX_MEM_alu_out_out,EX_MEM_data_out,
+    output[4:0] EX_MEM_rs2_ID_EX_out,EX_MEM_rd_out
 );
 
-    reg [63:0] alu_out_out_reg;
-    reg [63:0] data_out_reg;
-    reg [4:0] rd_out_reg;
-    reg mem_read_out_reg;
-    reg mem_to_reg_out_reg;
-    reg reg_write_en_out_reg;
-    reg mem_write_out_reg;
-    reg [4:0] rs2_ID_EX_out_reg;
+    reg [63:0] EX_MEM_alu_out_out_reg;
+    reg [63:0] EX_MEM_data_out_reg;
+    reg [4:0] EX_MEM_rd_out_reg;
+    reg EX_MEM_mem_read_out_reg;
+    reg EX_MEM_mem_to_reg_out_reg;
+    reg EX_MEM_reg_write_en_out_reg;
+    reg EX_MEM_mem_write_out_reg;
+    reg [4:0] EX_MEM_rs2_ID_EX_out_reg;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            alu_out_out_reg <= 64'b0;
-            data_out_reg <= 64'b0;
-            rd_out_reg <= 5'b0;
-            mem_read_out_reg <= 1'b0;
-            mem_to_reg_out_reg <= 1'b0;
-            reg_write_en_out_reg <= 1'b0;
-            mem_write_out_reg <= 1'b0;
-            rs2_ID_EX_out_reg <= 5'b0;
+            EX_MEM_alu_out_out_reg <= 64'b0;
+            EX_MEM_data_out_reg <= 64'b0;
+            EX_MEM_rd_out_reg <= 5'b0;
+            EX_MEM_mem_read_out_reg <= 1'b0;
+            EX_MEM_mem_to_reg_out_reg <= 1'b0;
+            EX_MEM_reg_write_en_out_reg <= 1'b0;
+            EX_MEM_mem_write_out_reg <= 1'b0;
+            EX_MEM_rs2_ID_EX_out_reg <= 5'b0;
         end else begin
-            alu_out_out_reg <= alu_out;
-            data_out_reg <= data;
-            rd_out_reg <= rd;
-            mem_read_out_reg <= mem_read;
-            mem_to_reg_out_reg <= mem_to_reg;
-            reg_write_en_out_reg <= reg_write_en;
-            mem_write_out_reg <= mem_write;
-            rs2_ID_EX_out_reg <= rs2_ID_EX;
+            EX_MEM_alu_out_out_reg <= EX_MEM_alu_out;
+            EX_MEM_data_out_reg <= EX_MEM_data;
+            EX_MEM_rd_out_reg <= EX_MEM_rd;
+            EX_MEM_mem_read_out_reg <= EX_MEM_mem_read;
+            EX_MEM_mem_to_reg_out_reg <= EX_MEM_mem_to_reg;
+            EX_MEM_reg_write_en_out_reg <= EX_MEM_reg_write_en;
+            EX_MEM_mem_write_out_reg <= EX_MEM_mem_write;
+            EX_MEM_rs2_ID_EX_out_reg <= EX_MEM_rs2_ID_EX;
         end
     end
 
-    assign alu_out_out = alu_out_out_reg;
-    assign data_out = data_out_reg;
-    assign rd_out = rd_out_reg;
-    assign mem_read_out = mem_read_out_reg;
-    assign mem_to_reg_out = mem_to_reg_out_reg;
-    assign reg_write_en_out = reg_write_en_out_reg;
-    assign mem_write_out = mem_write_out_reg;
-    assign rs2_ID_EX_out = rs2_ID_EX_out_reg;
+    assign EX_MEM_alu_out_out = EX_MEM_alu_out_out_reg;
+    assign EX_MEM_data_out = EX_MEM_data_out_reg;
+    assign EX_MEM_rd_out = EX_MEM_rd_out_reg;
+    assign EX_MEM_mem_read_out = EX_MEM_mem_read_out_reg;
+    assign EX_MEM_mem_to_reg_out = EX_MEM_mem_to_reg_out_reg;
+    assign EX_MEM_reg_write_en_out = EX_MEM_reg_write_en_out_reg;
+    assign EX_MEM_mem_write_out = EX_MEM_mem_write_out_reg;
+    assign EX_MEM_rs2_ID_EX_out = EX_MEM_rs2_ID_EX_out_reg;
 
 endmodule
