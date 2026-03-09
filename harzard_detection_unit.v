6d905108b464f9000651f0dab60b49be855a2bcf
poojith06 <saipoojith2006@gmail.com>
2026-03-09 09:50:17 +0530

    Update harzard_detection_unit.v module

--- harzard_detection_unit.v
+++ harzard_detection_unit.v
@@ -1,19 +1,34 @@
-module hazard_detection_unit(input idex_memread,input[4:0] idex_reg_rd,input[31:0] ifid_instruction,output reg pc_write,output reg ifid_write,output reg idex_flush)
+module hazard_detection_unit(
+    input ID_EX_mem_read,ld_sd_mem_write,ld_sd_mem_read,Branch_and_zero, 
+    input[4:0] IF_ID_rs1,IF_ID_rs2,ID_EX_rd,            
+    output pc_write,IF_ID_write,control_bubble_sel,flush         
+);
+    reg pc_write_reg, IF_ID_write_reg, control_bubble_sel_reg, flush_reg;
 
-wire[4:0] rs1=ifid_instruction[19:15];
-wire[4:0] rs2=ifid_instruction[24:20];
+    always @(*) begin
 
-always @(*) begin
-pc_write=1'b1;
-ifid_write=1'b1;
-idex_flush=1'b0;
+        pc_write_reg = 1'b1;
+        IF_ID_write_reg = 1'b1;
+        control_bubble_sel_reg = 1'b0;
+        flush_reg = 1'b0;
+        
+        if (Branch_and_zero) begin
+            flush_reg = 1'b1;
+        end
 
-if(idex_memread && ((idex_reg_rd ==rs1)||(idex_reg_rd ==rs2)) && (idex_reg_rd != 5'd0))
-begin
-pc_write=1'b0;
-ifid_write=1'b0;
-idex_flush=1'b1;
-end
-end
+
+        else if (ID_EX_mem_read && ((ID_EX_rd == IF_ID_rs1) || (ID_EX_rd == IF_ID_rs2 && !(ld_sd_mem_read || ld_sd_mem_write))) && (ID_EX_rd != 5'b0)) begin
+            
+            pc_write_reg = 1'b0;
+            IF_ID_write_reg = 1'b0;
+            control_bubble_sel_reg = 1'b1;
+            flush_reg = 1'b0;               
+        end
+    end
+
+    assign pc_write = pc_write_reg;
+    assign IF_ID_write = IF_ID_write_reg;
+    assign control_bubble_sel = control_bubble_sel_reg;
+    assign flush = flush_reg;
+
 endmodule
-
