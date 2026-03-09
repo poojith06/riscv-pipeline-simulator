eec28f6a4976627b39d3712df7327c71b8b2fec1
poojith06 <saipoojith2006@gmail.com>
2026-03-09 13:57:40 +0530

    Modified Forwarding_Unit.v module

--- Forwarding_Unit.v
+++ Forwarding_Unit.v
@@ -1,29 +1,26 @@
 module Forwarding_unit (
-    input wire [4:0] ID_EX_rs1,
-    input wire [4:0] ID_EX_rs2,
-    input wire [4:0] EX_MEM_rd,
-    input wire [4:0] MEM_WB_rd,
-    input wire       EX_MEM_Regwrite,
-    input wire       MEM_WB_Regwrite,
-
-    output reg [1:0] ForwardA,
-    output reg [1:0] ForwardB
+    input[4:0] ID_EX_rs1,ID_EX_rs2,EX_MEM_rd,MEM_WB_rd,
+    input EX_MEM_Regwrite,MEM_WB_Regwrite,
+    output[1:0] ForwardA,ForwardB
 );
+    reg[1:0] ForwardA_reg,ForwardB_reg;
     always @(*) begin
-        ForwardA = 2'b00;
-        ForwardB = 2'b00;
+        ForwardA_reg = 2'b00;
+        ForwardB_reg = 2'b00;
 
         if (EX_MEM_Regwrite && (EX_MEM_rd != 5'b0) && (EX_MEM_rd == ID_EX_rs1))
-            ForwardA = 2'b10;
+            ForwardA_reg = 2'b10;
         else if (MEM_WB_Regwrite && (MEM_WB_rd != 5'b0) && (MEM_WB_rd == ID_EX_rs1) &&
                  !(EX_MEM_Regwrite && (EX_MEM_rd == ID_EX_rs1)))
-            ForwardA = 2'b01;
+            ForwardA_reg = 2'b01;
 
         if (EX_MEM_Regwrite && (EX_MEM_rd != 5'b0) && (EX_MEM_rd == ID_EX_rs2))
-            ForwardB = 2'b10;
+            ForwardB_reg = 2'b10;
         else if (MEM_WB_Regwrite && (MEM_WB_rd != 5'b0) && (MEM_WB_rd == ID_EX_rs2) &&
                  !(EX_MEM_Regwrite && (EX_MEM_rd == ID_EX_rs2)))
-            ForwardB = 2'b01;
+            ForwardB_reg = 2'b01;
 
     end
+    assign ForwardA = ForwardA_reg;
+    assign ForwardB = ForwardB_reg;
 endmodule
