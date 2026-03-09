2b1c64accfb22d0e82e1496aab94f5ad832ae684
poojith06 <saipoojith2006@gmail.com>
2026-03-09 18:35:59 +0530

    Minor changes

--- control_bubble.v
+++ control_bubble.v
@@ -1,16 +1,16 @@
 module control_bubble(
-    input branch,mem_read,mem_to_reg,mem_write,alu_src,reg_write_en,control_bubble_sel,
-    input[3:0] op,
-    output branch_out,mem_read_out,mem_to_reg_out,mem_write_out,alu_src_out,reg_write_en_out,
-    output[3:0] op_out
+    input Branch_bubble,mem_read_bubble,mem_to_reg_bubble,mem_write_bubble,alu_src_bubble,reg_write_en_bubble,control_bubble_sel,
+    input[3:0] op_bubble,
+    output branch_out_bubble,mem_read_out_bubble,mem_to_reg_out_bubble,mem_write_out_bubble,alu_src_out_bubble,reg_write_en_out_bubble,
+    output[3:0] op_out_bubble
 );
 
-assign branch_out        = control_bubble_sel ? 1'b0 : branch;
-assign mem_read_out      = control_bubble_sel ? 1'b0 : mem_read;
-assign mem_to_reg_out    = control_bubble_sel ? 1'b0 : mem_to_reg;
-assign op_out            = control_bubble_sel ? 4'b0 : op;
-assign mem_write_out     = control_bubble_sel ? 1'b0 : mem_write;
-assign alu_src_out       = control_bubble_sel ? 1'b0 : alu_src;
-assign reg_write_en_out  = control_bubble_sel ? 1'b0 : reg_write_en;
+assign branch_out_bubble        = control_bubble_sel ? 1'b0 : Branch_bubble;
+assign mem_read_out_bubble      = control_bubble_sel ? 1'b0 : mem_read_bubble;
+assign mem_to_reg_out_bubble    = control_bubble_sel ? 1'b0 : mem_to_reg_bubble;
+assign op_out_bubble            = control_bubble_sel ? 4'b0 : op_bubble;
+assign mem_write_out_bubble     = control_bubble_sel ? 1'b0 : mem_write_bubble;
+assign alu_src_out_bubble       = control_bubble_sel ? 1'b0 : alu_src_bubble;
+assign reg_write_en_out_bubble  = control_bubble_sel ? 1'b0 : reg_write_en_bubble;
 
 endmodule
