c0f0986b3c1223845f76ec0a043747b579e6f5ee
poojith06 <saipoojith2006@gmail.com>
2026-03-09 16:53:26 +0530

    Minor changes

--- MEM_WB.v
+++ MEM_WB.v
@@ -1,38 +1,38 @@
 module MEM_WB(
-    input clk,reset,mem_to_reg,reg_write_en,
-    input[63:0] data,alu_out,
-    input[4:0] rd,
-    output mem_to_reg_out,reg_write_en_out,
-    output[63:0] data_out,alu_out_out,
-    output[4:0] rd_out
+    input clk,reset,MEM_WB_mem_to_reg,MEM_WB_reg_write_en,
+    input[63:0] MEM_WB_data,MEM_WB_alu_out,
+    input[4:0] MEM_WB_rd,
+    output MEM_WB_mem_to_reg_out,MEM_WB_reg_write_en_out,
+    output[63:0] MEM_WB_data_out,MEM_WB_alu_out_out,
+    output[4:0] MEM_WB_rd_out
 );
 
-    reg [63:0] alu_out_out_reg;
-    reg [63:0] data_out_reg;
-    reg [4:0] rd_out_reg;
-    reg mem_to_reg_out_reg;
-    reg reg_write_en_out_reg;
+    reg [63:0] MEM_WB_alu_out_out_reg;
+    reg [63:0] MEM_WB_data_out_reg;
+    reg [4:0] MEM_WB_rd_out_reg;
+    reg MEM_WB_mem_to_reg_out_reg;
+    reg MEM_WB_reg_write_en_out_reg;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            alu_out_out_reg <= 64'b0;
-            data_out_reg <= 64'b0;
-            rd_out_reg <= 5'b0;
-            mem_to_reg_out_reg <= 1'b0;
-            reg_write_en_out_reg <= 1'b0;
+            MEM_WB_alu_out_out_reg <= 64'b0;
+            MEM_WB_data_out_reg <= 64'b0;
+            MEM_WB_rd_out_reg <= 5'b0;
+            MEM_WB_mem_to_reg_out_reg <= 1'b0;
+            MEM_WB_reg_write_en_out_reg <= 1'b0;
         end else begin
-            alu_out_out_reg <= alu_out;
-            data_out_reg <= data;
-            rd_out_reg <= rd;
-            mem_to_reg_out_reg <= mem_to_reg;
-            reg_write_en_out_reg <= reg_write_en;
+            MEM_WB_alu_out_out_reg <= MEM_WB_alu_out;
+            MEM_WB_data_out_reg <= MEM_WB_data;
+            MEM_WB_rd_out_reg <= MEM_WB_rd;
+            MEM_WB_mem_to_reg_out_reg <= MEM_WB_mem_to_reg;
+            MEM_WB_reg_write_en_out_reg <= MEM_WB_reg_write_en;
         end
     end
 
-    assign alu_out_out = alu_out_out_reg;
-    assign data_out = data_out_reg;
-    assign rd_out = rd_out_reg;
-    assign mem_to_reg_out = mem_to_reg_out_reg;
-    assign reg_write_en_out = reg_write_en_out_reg;
-    
+    assign MEM_WB_alu_out_out = MEM_WB_alu_out_out_reg;
+    assign MEM_WB_data_out = MEM_WB_data_out_reg;
+    assign MEM_WB_rd_out = MEM_WB_rd_out_reg;
+    assign MEM_WB_mem_to_reg_out = MEM_WB_mem_to_reg_out_reg;
+    assign MEM_WB_reg_write_en_out = MEM_WB_reg_write_en_out_reg;
+
 endmodule
